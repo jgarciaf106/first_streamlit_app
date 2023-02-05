1666da7d7b9611215a901fd06c51aeb261ce1fb4
Andres Garcia <jgarciaf106@gmail.com>
2023-02-05 17:12:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 def get_fruitvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)  
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)  
 
     # format json resp to tabulat form
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
