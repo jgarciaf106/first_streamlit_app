a35ff273c53a926ff89406b9f6b79e18f8eca0b6
Andres Garcia <jgarciaf106@gmail.com>
2023-02-05 15:04:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.title("Mi Parents New Healthy Diner")
+streamlit.title("Mi Mom's New Healthy Diner")
 streamlit.header("Breakfast Favorites")
 
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
