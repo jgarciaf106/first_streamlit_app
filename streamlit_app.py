e993468d5ce460cbfd6aefe5eaa03d8d6a1268ee
Andres Garcia <jgarciaf106@gmail.com>
2023-02-05 17:07:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,22 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # format json resp to tabulat form
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
-# format json resp to tabulat form
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # Display table to the page.
+    streamlit.dataframe(fruityvice_normalized)
 
-# Display table to the page.
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 #snowflake connection
